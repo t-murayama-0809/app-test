e9c3d4a5eddc458da02f24377706f770d560e9e9
村山拓哉 <takuya@murayamatakuyanoMacBook-Air.local>
2025-10-02 16:25:35 +0900

    update app_test.py

--- app_test.py
+++ app_test.py
@@ -298,10 +298,10 @@ with st.expander("残価表"):
     try:
         df3['新車価格'] = (df3['max_price'] / 1000).fillna(0).round(0).astype(int)
         df3 = df3[df3['新車価格'] != 0]
-        df3['clr_adjusted_price'] = df3['clr_adjusted_price'].round(0).astype(int)
+        df3['clr_adjusted_price'] = df3['clr_adjusted_price'].fillna(0).round(0).astype(int)
         
         # --- 切替ボタン ---
-        mode = st.radio("price：（千円）、salvage_rate：（%）", ["price", "SV"], horizontal=True)
+        mode = st.radio("price：（千円）、salvage_rate：（%）", ["clr_adjusted_price", "SV"], horizontal=True)
 
         # --- ピボットテーブル作成 ---
         #pivot_table = pd.pivot_table(df3,values='price' if mode == "price" else 'RV',index=['grade','新車価格'],columns='nenss',aggfunc=lambda x:str(int(round(x.mean() * ratio ,0))))
